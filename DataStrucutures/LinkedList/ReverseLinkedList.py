646c9a4f4eeffc9f0e7e2c536f1b28caf76d204d
Dolapo Adedokun <adedokun@mit.edu>
2020-05-30 09:25:55 -0400

    Implemented ReverseListIterative

--- DataStrucutures/LinkedList/ReverseLinkedList.py
+++ DataStrucutures/LinkedList/ReverseLinkedList.py
@@ -8,7 +8,7 @@ Output: 5->4->3->2->1->NULL
 
 def reverseListRecursive(head):
     #A single node is already reversed, and a null node is also already reversed
-    if head.next == None or head == None:
+    if head == None or head.next == None:
         return head
     #assume function works and reverse the rest of the list
     reveresedRest = reverseListRecursive(head.next)
@@ -17,3 +17,23 @@ def reverseListRecursive(head):
     #set current node to point to None
     head.next = None
     return reveresedRest
+
+#RT: O(n) --> we visit all nodes in the list
+#SC: O(n) --> we store space in the stack for each node because of the
+#             recursive calls
+
+def reverseListIterative(head):
+    prev = None
+    curr = head
+    while curr:
+        #store next node before moving pointers
+        temp = curr.next
+        #set next node points to previous! this is reversing!
+        curr.next = prev
+        #now lets reset our prev and curr nodes for next iteration!
+        prev = curr
+        curr = temp
+    return prev
+
+#RT: O(n) --> we traverse all nodes in the graph
+#SC: O(1) --> we only store our two pointers to prev and curr.
